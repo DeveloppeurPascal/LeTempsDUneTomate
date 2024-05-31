41cab5a20329b3fa55ae9e66353f31a617ed7eb2
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-31 20:50:37 +0200

    mise à jour des constantes et de leurs valeurs par défaut

--- src/uConsts.pas
+++ src/uConsts.pas
@@ -4,13 +4,33 @@ interface
 
 const
   /// <summary>
-  /// Durée attendue d'un épisode
+  /// Durée attendue d'un épisode (en minutes)
   /// </summary>
+  /// <remarks>
+  /// Utiliser la valeur depuis TConfig ou TProject, pas la constante !
+  /// </remarks>
 {$IFDEF DEBUG}
-  CDureeEpisodeEnSecondes = 3 * 60;
+  // CDureeEpisodeEnMinutes = 3;
+  CDureeEpisodeEnMinutes = 25;
 {$ELSE}
-  CDureeEpisodeEnSecondes = 25 * 60;
+  CDureeEpisodeEnMinutes = 25;
 {$ENDIF}
+  /// <summary>
+  /// Default video width
+  /// </summary>
+  /// <remarks>
+  /// Utiliser la valeur depuis TConfig ou TProject, pas la constante !
+  /// </remarks>
+  CVideoWidth = 1920;
+
+  /// <summary>
+  /// Default video height
+  /// </summary>
+  /// <remarks>
+  /// Utiliser la valeur depuis TConfig ou TProject, pas la constante !
+  /// </remarks>
+  CVideoHeight = 1080;
+
   /// <summary>
   /// Durée en secondes repris sur l'épisode précédent en début du nouveau
   /// </summary>
@@ -32,16 +52,11 @@ const
   /// </summary>
   CDureeFin = 5;
 
-  CFFmpeg = '/Users/patrickpremartin/Downloads/ffmpeg-tests/ffmpeg';
-  CPrecedemment =
-    '/Users/patrickpremartin/Downloads/ffmpeg-tests/precedemment.png';
-  CPageIntro =
-    '/Users/patrickpremartin/Downloads/ffmpeg-tests/start-picture.png';
-  CPageFinEpisode =
-    '/Users/patrickpremartin/Downloads/ffmpeg-tests/end-picture.png';
-
-  CVideoWidth = 1920;
-  CVideoHeight = 1080;
+  // TODO : supprimer ces constantes liées et les laisser en fenêtre d'option du programme
+  CFFmpeg = '/Users/patrickpremartin/LeTempsDUneTomate/ffmpeg';
+  CPrecedemment = '/Users/patrickpremartin/LeTempsDUneTomate/precedemment.png';
+  CPageIntro = '/Users/patrickpremartin/LeTempsDUneTomate/start-picture.png';
+  CPageFinEpisode = '/Users/patrickpremartin/LeTempsDUneTomate/end-picture.png';
 
 implementation
 
