b170f799f9c80f2344cfb7ba1a342bd04885f968
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-31 18:53:45 +0200

    ajout de la politique de confidentialité standard, de la description du projet et du changement du titre de la fenêtre principale (#12)

--- src/fMain.pas
+++ src/fMain.pas
@@ -104,6 +104,8 @@ type
     function getConvertedCharImageIndex(Sender: TOlfFMXTextImageFrame;
       AChar: char): integer;
   public
+    procedure InitMainFormCaption;
+    procedure InitAboutDialogDescriptionAndLicense;
   end;
 
 var
@@ -433,6 +435,48 @@ begin
   end;
 end;
 
+procedure TfrmMain.InitAboutDialogDescriptionAndLicense;
+begin
+  OlfAboutDialog1.Licence.Text :=
+    'This program is distributed as shareware. If you use it (especially for ' +
+    'commercial or income-generating purposes), please remember the author and '
+    + 'contribute to its development by purchasing a license.' + slinebreak +
+    slinebreak +
+    'This software is supplied as is, with or without bugs. No warranty is offered '
+    + 'as to its operation or the data processed. Make backups!';
+  OlfAboutDialog1.Description.Text :=
+    'Program for cutting videos into 20-25 minute episodes in pomodoro mode.' +
+    slinebreak + slinebreak + '*****************' + slinebreak +
+    '* Publisher info' + slinebreak + slinebreak +
+    'This application was developed by Patrick Prémartin.' + slinebreak +
+    slinebreak +
+    'It is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.'
+    + slinebreak + slinebreak + '****************' + slinebreak +
+    '* Personal data' + slinebreak + slinebreak +
+    'This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.'
+    + slinebreak + slinebreak + 'We have no knowledge of what you do with it.' +
+    slinebreak + slinebreak +
+    'No information about you is transmitted to us or to any third party.' +
+    slinebreak + slinebreak +
+    'We use no cookies, no tracking, no stats on your use of the application.' +
+    slinebreak + slinebreak + '**********************' + slinebreak +
+    '* User support' + slinebreak + slinebreak +
+    'If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.'
+    + slinebreak + slinebreak + 'To find out more, visit ' +
+    OlfAboutDialog1.URL;
+end;
+
+procedure TfrmMain.InitMainFormCaption;
+begin
+{$IFDEF DEBUG}
+  caption := '[DEBUG] ';
+{$ELSE}
+  caption := '';
+{$ENDIF}
+  caption := caption + OlfAboutDialog1.Titre + ' v' +
+    OlfAboutDialog1.VersionNumero;
+end;
+
 procedure TfrmMain.ExecuteFFmpegAndWait(const AParams,
   DestinationFilePath: string);
 var
@@ -458,8 +502,9 @@ end;
 
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
-  caption := OlfAboutDialog1.Titre + ' v' + OlfAboutDialog1.VersionNumero;
-  // TODO : à compléter (debug, nom projet, ...)
+  InitMainFormCaption;
+
+  InitAboutDialogDescriptionAndLicense;
 end;
 
 procedure TfrmMain.OlfAboutDialog1URLClick(const AURL: string);
