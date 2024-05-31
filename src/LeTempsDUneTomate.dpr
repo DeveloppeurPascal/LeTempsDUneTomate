ed38adcaf61f1053c6a3e73373f145f4863f0a24
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-31 20:51:37 +0200

    ajout de nouvelles unités (configuration et chiffrement)

--- src/LeTempsDUneTomate.dpr
+++ src/LeTempsDUneTomate.dpr
@@ -8,12 +8,15 @@ uses
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   Olf.RTL.Language in '..\lib-externes\librairies\src\Olf.RTL.Language.pas',
-  Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
   u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
   uConsts in 'uConsts.pas',
   udmAdobeStock_286917767 in '..\_PRIVATE\AdobeStock_286917767\udmAdobeStock_286917767.pas' {dmAdobeStock_286917767: TDataModule},
   Olf.FMX.TextImageFrame in '..\lib-externes\librairies\src\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame},
-  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule};
+  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule},
+  Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
+  uConfig in 'uConfig.pas',
+  Olf.RTL.CryptDecrypt in '..\lib-externes\librairies\src\Olf.RTL.CryptDecrypt.pas',
+  uProject in 'uProject.pas';
 
 {$R *.res}
 
