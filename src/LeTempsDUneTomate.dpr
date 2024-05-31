ad0093398177737ae53b127a0a27bb01c1fdb52a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-31 18:43:34 +0200

    added a data module for the logo and updated the forms creation order

--- src/LeTempsDUneTomate.dpr
+++ src/LeTempsDUneTomate.dpr
@@ -12,13 +12,15 @@ uses
   u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
   uConsts in 'uConsts.pas',
   udmAdobeStock_286917767 in '..\_PRIVATE\AdobeStock_286917767\udmAdobeStock_286917767.pas' {dmAdobeStock_286917767: TDataModule},
-  Olf.FMX.TextImageFrame in '..\lib-externes\librairies\src\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame};
+  Olf.FMX.TextImageFrame in '..\lib-externes\librairies\src\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame},
+  uDMLogo in 'uDMLogo.pas' {dmLogo: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TfrmMain, frmMain);
   Application.CreateForm(TdmAdobeStock_286917767, dmAdobeStock_286917767);
+  Application.CreateForm(TdmLogo, dmLogo);
+  Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
 end.
