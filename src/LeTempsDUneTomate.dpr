08d3b1c2aa3014f95e7a88bc3b0d4f0b2ee501e6
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-19 13:36:59 +0200

    added copyright and timestamp comments

--- src/LeTempsDUneTomate.dpr
+++ src/LeTempsDUneTomate.dpr
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Le temps d'une tomate
+
+  Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://developpeur-pascal.fr/le-temps-d-une-tomate.html
+
+  Project site :
+  https://github.com/DeveloppeurPascal/LeTempsDUneTomate
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:42:09.116+02:00
+  Signature : 074477a34c79eb05110c6cbc35e302d05266b418
+  ***************************************************************************
+*)
+
 program LeTempsDUneTomate;
 
 uses
--- src/uDMLogo.pas
+++ src/uDMLogo.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  Le temps d'une tomate
+
+  Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://developpeur-pascal.fr/le-temps-d-une-tomate.html
+
+  Project site :
+  https://github.com/DeveloppeurPascal/LeTempsDUneTomate
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:42:09.123+02:00
+  Signature : 7954ffd50cb7d32671fd45e1d6d81b3b963f0d8e
+  ***************************************************************************
+*)
+
 unit uDMLogo;
 
 interface
